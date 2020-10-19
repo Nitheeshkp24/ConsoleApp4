55d8609e2c014d841b9a67308c93ab41a818a5d3
nitheeshkp24@gmail.com <nitheeshkp24@gmail.com>
2020-10-19 12:08:09 +0530

    Overriding

---
+++ OverRiding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OverRiding
+{
+
+   class overr
+   {
+        public void Identity(string name,int id)
+        {
+            Console.WriteLine("Name: "+name +"      "+"id:  "+id);
+        }
+        public void Identity( int id, string name)
+        {
+            Console.WriteLine("Name: " + name + "      " + "id:  " + id);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            overr obj = new overr();
+            obj.Identity("Nitheesh", 2);
+            obj.Identity( 1, "Nithya");
+
+
+        }
+    }
+}
