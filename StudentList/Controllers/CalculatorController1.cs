80211a3405640bef6243da33ff2d26a375416eb4
nitheeshkp24@gmail.com <nitheeshkp24@gmail.com>
2020-10-21 21:44:13 +0530

    web app

---
+++ StudentList/Controllers/CalculatorController1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentList.Controllers
+{
+    public class CalculatorController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add(int n1,int n2)
+        {
+            ViewBag.result = n1 + n2;
+            return View();
+        }
+
+    }
+}
---
+++ StudentList/Controllers/index.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentList.Controllers
+{
+    public class index : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddStudent(string n1,string address)
+        {
+            ViewBag.name = n1;
+            ViewBag.address = address;
+
+            return View();
+        }
+    }
+}
